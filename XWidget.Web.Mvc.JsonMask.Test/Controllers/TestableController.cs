2070de39e12848393d4834e56409b30e8f82952e
XuPeiYao <st89382000@gmail.com>
2018-06-17 20:08:31 +0800

    加入控制器型別屏蔽選項

---
+++ XWidget.Web.Mvc.JsonMask.Test/Controllers/TestableController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using XWidget.Web.Mvc.JsonMask.Test.Models;
+
+namespace XWidget.Web.Mvc.JsonMask.Test.Controllers {
+    public class TestableController : Controller {
+        //[HttpGet("TestByController")]
+
+        public object Result { get; set; }
+
+        public override void OnActionExecuting(ActionExecutingContext context) {
+            base.OnActionExecuting(context);
+        }
+
+        public void TestByController() {
+            Result = this.Mask(Category_Controller.GetCategories());
+        }
+    }
+}
--- XWidget.Web.Mvc.JsonMask.Test/JsonMaskTest.cs
+++ XWidget.Web.Mvc.JsonMask.Test/JsonMaskTest.cs
@@ -1,22 +1,37 @@
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Routing;
 using System;
+using System.Linq;
 using Xunit;
+using XWidget.Web.Mvc.JsonMask.Test.Controllers;
 using XWidget.Web.Mvc.JsonMask.Test.Models;
 
 namespace XWidget.Web.Mvc.JsonMask.Test {
     public class JsonMaskTest {
+        /// <summary>
+        /// 依據模式名稱屏蔽
+        /// </summary>
         [Fact]
-        public void ByPattern() {
-            var maskedResult = ControllerExtension.Mask(null, Category.GetCategories(), "MaskName");
+        public void ByPatternName() {
+            var maskedResult = ControllerExtension.Mask(null, Category_PatternName.GetCategories(), "MaskName");
 
             foreach (var category in maskedResult) {
-                Assert.Null(category.Name);
+                Assert.Null(category.Children);
             }
         }
 
+        /// <summary>
+        /// 依據控制器屏蔽
+        /// </summary>
         [Fact]
-        public void ByDeclaringType() {
-            var maskedResult = ControllerExtension.Mask(null, Category.GetCategories());
+        public void ByController() {
+            var controller = new TestableController();
+
+            var maskedResult = controller.Mask(Category_Controller.GetCategories());
 
             foreach (var category in maskedResult) {
                 Assert.Null(category.Children);
