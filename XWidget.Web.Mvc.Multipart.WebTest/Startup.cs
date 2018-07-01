b82ff3ea35bf48d9ee96bf36ef3340e104761ca3
XuPeiYao <st89382000@gmail.com>
2018-07-02 00:30:34 +0800

    加入Multipart專案與MVC測試專案

---
+++ XWidget.Web.Mvc.Multipart.WebTest/Startup.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace XWidget.Web.Mvc.Multipart.WebTest {
+    public class Startup {
+        public Startup(IConfiguration configuration) {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services) {
+            services.AddMvc(options => {
+                options.ModelBinderProviders.Insert(0, new Multipart.MultipartJsonModelBinderProvider());
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+            if (env.IsDevelopment()) {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
---
+++ XWidget.Web.Mvc.Multipart/FromJsonAttribute.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace XWidget.Web.Mvc.Multipart {
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter)]
+    public class FromJsonAttribute : FromFormAttribute {
+
+    }
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter)]
+    public class FromJson2Attribute : Attribute {
+
+    }
+}
