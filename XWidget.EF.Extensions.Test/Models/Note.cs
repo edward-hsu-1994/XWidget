0905e18073b2dd53ff1a202da573dbc1d43780ae
XuPeiYao <st89382000@gmail.com>
2018-07-10 23:25:02 +0800

    加入連鎖刪除單元測試

---
+++ XWidget.EF.Extensions.Test/Models/Note.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XWidget.EF.Extensions.Test.Models {
+    public class Note {
+        public Note() {
+            Id = Guid.NewGuid();
+        }
+        public virtual Guid Id { get; set; }
+        public virtual Guid CategoryId { get; set; }
+        public virtual string Title { get; set; }
+        public virtual string Context { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
