20bd6f46f9aec3bcd0f84d3b3f78ea01350fbd4c
XuPeiYao <st89382000@gmail.com>
2018-07-21 01:34:59 +0800

    加入支援IQueryable與IEnumerable的最大值最小值取得擴充方法

---
+++ XWidget.Linq.Test/MaxMinExtensionTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace XWidget.Linq.Test {
+    public class MaxMinExtensionTest {
+        [Fact(DisplayName = "MaxMinExtensionTest.MaxOrDefault")]
+        public void MaxOrDefault() {
+            Assert.Equal(100, Enumerable.Range(1, 100).MaxOrDefault(x => x));
+        }
+
+        [Fact(DisplayName = "MaxMinExtensionTest.MinOrDefault")]
+        public void MinOrDefault() {
+            Assert.Equal(1, Enumerable.Range(1, 100).MinOrDefault(x => x));
+        }
+    }
+}
