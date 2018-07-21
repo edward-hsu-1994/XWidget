5a210aabb1b16f1169b668fc55476d60c6d44425
XuPeiYao <st89382000@gmail.com>
2018-07-21 11:26:14 +0800

    加入最大最小值預設參數

--- XWidget.Linq.Test/MaxMinExtensionTest.cs
+++ XWidget.Linq.Test/MaxMinExtensionTest.cs
@@ -9,11 +9,19 @@ namespace XWidget.Linq.Test {
         [Fact(DisplayName = "MaxMinExtensionTest.MaxOrDefault")]
         public void MaxOrDefault() {
             Assert.Equal(100, Enumerable.Range(1, 100).MaxOrDefault(x => x));
+
+            Assert.Equal(default(int), Enumerable.Range(1, 0).MaxOrDefault(x => x));
+
+            Assert.Null(Enumerable.Range(1, 0).MaxOrDefault(x => x, null));
         }
 
         [Fact(DisplayName = "MaxMinExtensionTest.MinOrDefault")]
         public void MinOrDefault() {
             Assert.Equal(1, Enumerable.Range(1, 100).MinOrDefault(x => x));
+
+            Assert.Equal(default(int), Enumerable.Range(1, 0).MaxOrDefault(x => x));
+
+            Assert.Null(Enumerable.Range(1, 0).MinOrDefault(x => x, null));
         }
     }
 }
--- XWidget.Linq/MaxMinExpressionExtension.cs
+++ XWidget.Linq/MaxMinExpressionExtension.cs
@@ -16,6 +16,7 @@ namespace System.Linq {
         /// <typeparam name="TSource">元素類別</typeparam>
         /// <typeparam name="TKey">排序主鍵類別</typeparam>
         /// <param name="source">目前實例</param>
+        /// <param name="selector">選擇器</param>
         /// <returns>最大值或預設值</returns>
         public static TKey MaxOrDefault<TSource, TKey>(this IQueryable<TSource> source, Expression<Func<TSource, TKey>> selector) {
             return source.OrderByDescending(selector).Select(selector).FirstOrDefault();
@@ -27,9 +28,46 @@ namespace System.Linq {
         /// <typeparam name="TSource">元素類別</typeparam>
         /// <typeparam name="TKey">排序主鍵類別</typeparam>
         /// <param name="source">目前實例</param>
+        /// <param name="selector">選擇器</param>
         /// <returns>最小值或預設值</returns>
         public static TKey MinOrDefault<TSource, TKey>(this IQueryable<TSource> source, Expression<Func<TSource, TKey>> selector) {
             return source.OrderBy(selector).Select(selector).FirstOrDefault();
+        }
+
+
+        /// <summary>
+        /// 取得選擇屬性最大值或預設值
+        /// </summary>
+        /// <typeparam name="TSource">元素類別</typeparam>
+        /// <typeparam name="TKey">排序主鍵類別</typeparam>
+        /// <param name="source">目前實例</param>
+        /// <param name="selector">選擇器</param>
+        /// <param name="defaultValue">預設值</param>
+        /// <returns>最大值或預設值</returns>
+        public static Nullable<TKey> MaxOrDefault<TSource, TKey>(
+            this IQueryable<TSource> source,
+            Expression<Func<TSource, TKey>> selector,
+            Nullable<TKey> defaultValue)
+            where TKey : struct {
+            if (!source.Any()) return defaultValue;
+            return source.MaxOrDefault(selector);
+        }
+
+        /// <summary>
+        /// 取得選擇屬性最小值或預設值
+        /// </summary>
+        /// <typeparam name="TSource">元素類別</typeparam>
+        /// <typeparam name="TKey">排序主鍵類別</typeparam>
+        /// <param name="source">目前實例</param>
+        /// <param name="selector">選擇器</param>
+        /// <returns>最小值或預設值</returns>
+        public static Nullable<TKey> MinOrDefault<TSource, TKey>(
+            this IQueryable<TSource> source,
+            Expression<Func<TSource, TKey>> selector,
+            Nullable<TKey> defaultValue)
+            where TKey : struct {
+            if (!source.Any()) return defaultValue;
+            return source.MaxOrDefault(selector);
         }
     }
 }
