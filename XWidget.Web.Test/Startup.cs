e259fe9209ff01c5a7d7e60ce7c824e1b9e42742
XuPeiYao <st89382000@gmail.com>
2018-07-23 23:56:35 +0800

    加入XWidget.Web單元測試專案

---
+++ XWidget.Web.Test/Startup.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using XWidget.Web.Test.Controllers;
+
+namespace XWidget.Web.Test {
+    public class Startup {
+        public Startup(IConfiguration configuration) {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services) {
+            services.EnableRangeRequest();
+
+            services.AddScoped(typeof(Nullable<bool>), x => true);
+
+            services.AddMvc()
+                .AddApplicationPart(typeof(TestController).GetTypeInfo().Assembly)
+                .AddControllersAsServices();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+            if (env.IsDevelopment()) {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseServiceHelper();
+
+            app.UseNoCache();
+
+            app.UseMvc();
+        }
+    }
+}
