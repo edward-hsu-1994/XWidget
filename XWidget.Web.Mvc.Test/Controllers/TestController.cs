18e35132457c0619514adc2d94143cbfc8424097
XuPeiYao <st89382000@gmail.com>
2018-07-24 23:21:03 +0800

    加入XWidget.Web.Mvc單元測試專案

---
+++ XWidget.Web.Mvc.Test/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace XWidget.Web.Mvc.Test.Controllers {
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase {
+        [HttpGet]
+        public string Test() {
+            throw new Exception("Test Error");
+        }
+
+        [HttpGet("list")]
+        public PaginationResult<IEnumerable<int>> List() {
+            return Paging(Enumerable.Range(0, 100), 0, 10);
+        }
+    }
+}
---
+++ XWidget.Web.Mvc.Test/WebTest.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace XWidget.Web.Mvc.Test {
+    public class WebTest {
+        [Fact]
+        public async Task ExceptionAndPagingTest() {
+            var webhost = BuildWebHost(new string[0]);
+
+            await webhost.StartAsync();
+
+            var client = new HttpClient();
+
+            var response1 = await client.GetAsync("http://localhost:9996/api/test");
+
+            Assert.False(response1.IsSuccessStatusCode);
+
+            var response2 = await client.GetAsync("http://localhost:9996/api/test/list");
+
+            Assert.True(response2.IsSuccessStatusCode);
+
+            var response2Content = JObject.Parse(await response2.Content.ReadAsStringAsync()).ToObject<PaginationResult<IEnumerable<int>>>();
+
+            Assert.Equal(response2Content.Result.Count(), 10);
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseKestrel(options => {
+                    options.Listen(IPAddress.Loopback, 9996);
+                })
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
