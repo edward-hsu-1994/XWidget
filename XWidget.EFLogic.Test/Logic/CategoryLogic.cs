5ea78855156f6765719569be4c80b12f9b8784b1
XuPeiYao <st89382000@gmail.com>
2018-07-30 22:44:00 +0800

    EFLogic支援完整的DI

---
+++ XWidget.EFLogic.Test/Logic/CategoryLogic.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using XWidget.EFLogic.Test.Models;
+
+namespace XWidget.EFLogic.Test.Logic {
+    public class CategoryLogic : LogicBase<TestContext, Category, Guid> {
+        public CategoryLogic(TestLogicManager manager) : base(manager) {
+
+        }
+    }
+}
--- XWidget.EFLogic.Test/Logic/TestLogicManager.cs
+++ XWidget.EFLogic.Test/Logic/TestLogicManager.cs
@@ -5,6 +5,8 @@ using XWidget.EFLogic.Test.Models;
 
 namespace XWidget.EFLogic.Test.Logic {
     public class TestLogicManager : LogicManagerBase<TestContext> {
+        public NoteLogic Note { get; set; }
+        public CategoryLogic Category { get; set; }
 
         public TestLogicManager(TestContext dbContext) : base(dbContext) { }
 
