a39f0d87f8078798f40e8ba343c8556553f30239
XuPeiYao <st89382000@gmail.com>
2018-07-31 00:11:33 +0800

    EFLogic中的Manager的DI改用.net core內建DI框架

---
+++ XWidget.EFLogic/InternalLogicManagerContainer.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XWidget.EFLogic {
+    internal class InternalLogicManagerContainer<TContext>
+        where TContext : DbContext {
+        public LogicManagerBase<TContext> Manager { get; set; }
+    }
+}
