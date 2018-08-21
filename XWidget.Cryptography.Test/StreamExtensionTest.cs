44d1697ddb252c37b860ce1b04280b03bcbfd626
XuPeiYao <st89382000@gmail.com>
2018-08-21 20:07:38 +0800

    添加Crytography測試

--- XWidget.Cryptography.Test/StreamExtensionTest.cs
+++ XWidget.Cryptography.Test/StreamExtensionTest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Security.Cryptography;
 using System.Text;
 using Xunit;
+using XWidget.Utilities;
 
 namespace XWidget.Cryptography.Test {
     public class StreamExtensionTest {
@@ -15,6 +16,20 @@ namespace XWidget.Cryptography.Test {
 
             using (var file = System.IO.File.Open(path, System.IO.FileMode.Open)) {
                 Assert.Equal("81dc9bdb52d04dc20036dbd8313ed055", file.ToHashString<MD5>(false));
+            }
+
+            System.IO.File.Delete(path);
+        }
+
+        [Fact(DisplayName = "StreamExtension.ToHash")]
+        public void ToHash() {
+            var path = Guid.NewGuid().ToString() + ".txt";
+            using (var stream = System.IO.File.CreateText(path)) {
+                stream.Write("1234");
+            }
+
+            using (var file = System.IO.File.Open(path, System.IO.FileMode.Open)) {
+                Assert.Equal(ByteUtility.FromHex("81dc9bdb52d04dc20036dbd8313ed055"), file.ToHash<MD5>());
             }
 
             System.IO.File.Delete(path);
--- XWidget.Cryptography.Test/StringExtensionTest.cs
+++ XWidget.Cryptography.Test/StringExtensionTest.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Security.Cryptography;
 using Xunit;
+using XWidget.Utilities;
 
 namespace XWidget.Cryptography.Test {
     public class StringExtensionTest {
@@ -11,6 +12,15 @@ namespace XWidget.Cryptography.Test {
         public void ToHashString(string str, string result) {
             Assert.Equal(str.ToHashString<MD5>(true), result.ToUpper());
             Assert.Equal(str.ToHashString<MD5>(false), result.ToLower());
+        }
+
+        [Theory(DisplayName = "StringExtension.ToHashString")]
+        [InlineData("1234", "81dc9bdb52d04dc20036dbd8313ed055")]
+        [InlineData("abc", "900150983cd24fb0d6963f7d28e17f72")]
+        [InlineData("dddd", "11ddbaf3386aea1f2974eee984542152")]
+        public void ToHash(string str, string result) {
+            Assert.Equal(str.ToHash<MD5>(), ByteUtility.FromHex(result));
+            Assert.Equal(str.ToHash<MD5>(), ByteUtility.FromHex(result));
         }
     }
 }
