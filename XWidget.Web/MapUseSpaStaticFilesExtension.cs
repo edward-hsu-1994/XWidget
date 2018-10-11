49194625519a869212a9c8fa009f2b4eec791f06
XuPeiYao <st89382000@gmail.com>
2018-10-11 22:16:15 +0800

    加入附帶修改index.html中baseHref的多載

--- XWidget.Web/MapUseSpaStaticFilesExtension.cs
+++ XWidget.Web/MapUseSpaStaticFilesExtension.cs
@@ -36,5 +36,64 @@ namespace XWidget.Web {
                 });
             });
         }
+
+        /// <summary>
+        /// 在指令路由使用指定檔案來源的SPA且自動修正index.html中的Base元素
+        /// </summary>
+        /// <param name="app">應用程式建構器</param>
+        /// <param name="pathMatch">子路由</param>
+        /// <param name="options">靜態檔案選項</param>
+        /// <param name="configuration">子路由之應用程式建構流程</param>
+        /// <returns>應用程式建構器</returns>
+        public static IApplicationBuilder MapUseSpaStaticFilesWithFixBaseHref(
+            this IApplicationBuilder app,
+            PathString pathMatch,
+            StaticFileOptions options,
+            Action<IApplicationBuilder> configuration = null
+            ) {
+            return app.MapUseSpaStaticFiles(
+                pathMatch,
+                options,
+                app2 => {
+                    // 複寫BaseHref
+                    app2.Use(async (context, next) => {
+                        var originStream = context.Response.Body;
+                        var warpStream = new MemoryStream();
+
+                        context.Response.Body = warpStream;
+                        await next();
+
+                        if (context.Response.ContentType == "text/html" &&
+                           context.Response.StatusCode == StatusCodes.Status200OK) {
+                            warpStream.Seek(0, SeekOrigin.Begin);
+                            // 讀取HTML內容
+                            var rawHtml = await new StreamReader(warpStream).ReadToEndAsync();
+                            // 剖析HTML
+                            HtmlDocument html = new HtmlDocument();
+                            html.LoadHtml(rawHtml);
+
+                            // 取得BaseElement並設定href
+                            var baseNode = html.DocumentNode.SelectSingleNode("//base");
+                            if (baseNode != null) {
+                                baseNode.SetAttributeValue("href", pathMatch + "/");
+                            }
+
+                            warpStream = new MemoryStream();
+                            StreamWriter streamWriter = new StreamWriter(warpStream);
+                            streamWriter.Write(html.DocumentNode.OuterHtml);
+                            streamWriter.Flush();
+                        }
+                        warpStream.Seek(0, SeekOrigin.Begin);
+
+                        context.Response.Body = originStream;
+                        context.Response.Clear();
+
+                        await warpStream.CopyToAsync(context.Response.Body);
+                        context.Response.ContentLength = warpStream.Length;
+                    });
+
+                    configuration?.Invoke(app2);
+                });
+        }
     }
 }
