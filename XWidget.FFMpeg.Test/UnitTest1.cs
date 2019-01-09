b5b5d5ca947d8d95580bd16f409551daaf2b91f4
XuPeiYao <st89382000@gmail.com>
2019-01-09 22:42:56 +0800

    基礎實現FFMPEG參數產生

---
+++ XWidget.FFMpeg.Test/UnitTest1.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+
+namespace XWidget.FFMpeg.Test {
+    public class UnitTest1 {
+        [Fact]
+        public void Test1() {
+            var builder = new FFMpegConverterBuilder();
+            builder
+                .ConfigVideo(option =>
+                    option.SetSize(352, 240).SetPreset(VideoOption.Preset.veryfast).SetCrf(23)
+                )
+                .ConfigAudio(option =>
+                    option.SetChannels(1).SetFrequency(16 * 1000)
+                );
+
+            var kk = builder.BuildArgs();
+        }
+    }
+}
---
+++ XWidget.FFMpeg/FFMpegConverterBuilder.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace XWidget.FFMpeg {
+    public class FFMpegConverterBuilder {
+        private GenericOption generic = new GenericOption();
+        private VideoOption video = new VideoOption();
+        private AudioOption audio = new AudioOption();
+
+        public FFMpegConverterBuilder ConfigGeneric(Action<GenericOption> genericConfig) {
+            genericConfig(generic);
+            return this;
+        }
+
+        public FFMpegConverterBuilder ConfigVideo(Action<VideoOption> videoConfig) {
+            videoConfig(video);
+            return this;
+        }
+
+        public FFMpegConverterBuilder ConfigAudio(Action<AudioOption> audioConfig) {
+            audioConfig(audio);
+            return this;
+        }
+
+        public string BuildArgs() {
+            var generic_str = string.Join(" ", generic.args.Select(x => $"-{x.Key} {x.Value ?? ""}"));
+            var video_str = string.Join(" ", video.args.Select(x => $"-{x.Key} {x.Value ?? ""}"));
+            var audio_str = string.Join(" ", audio.args.Select(x => $"-{x.Key} {x.Value ?? ""}"));
+
+            return string.Join(" ", generic_str, video_str, audio_str);
+        }
+
+        public IFFMpegConverter Build() {
+            var generic_str = string.Join(" ", generic.args.Select(x => $"-{x.Key} {x.Value ?? ""}"));
+            var video_str = string.Join(" ", video.args.Select(x => $"-{x.Key} {x.Value ?? ""}"));
+            var audio_str = string.Join(" ", audio.args.Select(x => $"-{x.Key} {x.Value ?? ""}"));
+
+
+
+            return null;
+        }
+    }
+}
