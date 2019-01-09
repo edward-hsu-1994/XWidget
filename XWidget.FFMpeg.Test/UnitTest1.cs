f8e4e625b4d1721d0a74dbef9e93a9349429b6f1
XuPeiYao <st89382000@gmail.com>
2019-01-09 22:49:30 +0800

    加入聲音比特率測試項目並加入註解

--- XWidget.FFMpeg.Test/UnitTest1.cs
+++ XWidget.FFMpeg.Test/UnitTest1.cs
@@ -7,14 +7,14 @@ namespace XWidget.FFMpeg.Test {
         public void Test1() {
             var builder = new FFMpegConverterBuilder();
             builder
-                .ConfigGeneric(option =>
+                .ConfigGeneric(option => //跳過40秒取10秒
                     option.SetStartPosition(40).SetDuration(10)
                 )
-                .ConfigVideo(option =>
+                .ConfigVideo(option => //設定新尺寸且轉換速度設為非常快，調整品質為23
                     option.SetSize(352, 240).SetPreset(VideoOption.Preset.veryfast).SetCrf(23)
                 )
-                .ConfigAudio(option =>
-                    option.SetChannels(1).SetFrequency(16 * 1000)
+                .ConfigAudio(option => //設定聲音取樣率為16K且為單聲道，比特率為32K
+                    option.SetChannels(1).SetFrequency(16 * 1000).SetBitrate(32 * 1000)
                 );
 
             var kk = builder.BuildArgs();
