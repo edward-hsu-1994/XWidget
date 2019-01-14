5ed35fb69e370631fd6c75e14c62d28018d29475
XuPeiYao <st89382000@gmail.com>
2019-01-15 00:09:18 +0800

    加入連鎖刪除設定Attribute

---
+++ XWidget.EFLogic/RemoveCascadeAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XWidget.EFLogic {
+    /// <summary>
+    /// 連鎖刪除模式
+    /// </summary>
+    public enum RemoveCascadeMode {
+        /// <summary>
+        /// 僅選擇項目不連鎖刪除
+        /// </summary>
+        OptOut,
+        /// <summary>
+        /// 僅選擇項目可連鎖
+        /// </summary>
+        OptIn,
+    }
+
+    /// <summary>
+    /// 連鎖刪除設定
+    /// </summary>
+    public class RemoveCascadeAttribute : Attribute {
+        /// <summary>
+        /// 連鎖刪除模式
+        /// </summary>
+        public RemoveCascadeMode Mode { get; set; } = RemoveCascadeMode.OptOut;
+    }
+}
