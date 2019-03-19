95cd8584cb7522e817e5170ed04c934ae27a68af
XuPeiYao <st89382000@gmail.com>
2019-03-19 22:12:53 +0800

    修正建構腳本以及專案設定

--- build.sh
+++ build.sh
@@ -1,6 +1,13 @@
 # Read Version
 version=$(cat version)
 
+# Output Path
+path=$(pwd)
+path="$path/ngpkgs"
+
+# Remove Old Output
+rm -R -f $path
+
 # Restore Projects
 find . -type d | grep '^./XWidget.[^/]*$' | { while read -r project; do eval "dotnet restore $project;"; done }
 
@@ -8,4 +15,4 @@ find . -type d | grep '^./XWidget.[^/]*$' | { while read -r project; do eval "do
 find . -type d | grep '^./XWidget.[^/]*$' | grep '\b\.Test$' | { while read -r project; do eval "dotnet test $project;"; done }
 
 # Pack
-find . -type d | grep '^./XWidget.[^/]*$' | grep -v '\b\.Test$' | { while read -r project; do eval "dotnet pack $project --version-suffix=$version --output ./ngpkgs; "; done }
+find . -type d | grep '^./XWidget.[^/]*$' | grep -v '\b\.Test$' | { while read -r project; do eval "dotnet pack $project -p:Version=$version --output $path; "; done }
