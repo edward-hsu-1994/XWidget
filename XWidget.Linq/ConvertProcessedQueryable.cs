fe6e7679d250cc55769d577a0341189f66076e13
XuPeiYao <st89382000@gmail.com>
2019-03-20 17:15:44 +0800

    加入可轉換查詢列舉相關類別與擴充方法

---
+++ XWidget.Linq/ConvertProcessedQueryable.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace XWidget.Linq {
+    /// <summary>
+    /// 可轉換查詢列舉
+    /// </summary>
+    /// <typeparam name="Tin">輸入元素類型</typeparam>
+    /// <typeparam name="Tout">輸出元素類型</typeparam>
+    public class ConvertProcessedQueryable<Tin, Tout> : IQueryable<Tout> {
+        public IQueryable<Tin> Source { get; internal set; }
+
+        public Func<Tin, Tout> Process { get; internal set; }
+
+        public Type ElementType => Source.ElementType;
+
+        public Expression Expression => Source.Expression;
+
+        public IQueryProvider Provider => Source.Provider;
+
+        public IEnumerator<Tout> GetEnumerator() {
+            return new ConvertProcessedEnumerator<Tin, Tout>() {
+                Source = Source.GetEnumerator(),
+                Process = Process
+            };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ XWidget.Linq/ConvertProcessedQueryableExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XWidget.Linq {
+    public static class ConvertProcessedQueryableExtension {
+        /// <summary>
+        /// 查詢轉換處理
+        /// </summary>
+        /// <typeparam name="Tin">輸入元素類型</typeparam>
+        /// <typeparam name="Tout">輸出元素類型</typeparam>
+        /// <param name="obj">查詢</param>
+        /// <param name="process">處理程序</param>
+        /// <returns>隱含處理的查詢物件</returns>
+        public static ConvertProcessedQueryable<Tin, Tout> ConvertProcess<Tin, Tout>(this IQueryable<Tin> obj, Func<Tin, Tout> process) {
+            return new ConvertProcessedQueryable<Tin, Tout>() {
+                Source = obj,
+                Process = process
+            };
+        }
+    }
+}
