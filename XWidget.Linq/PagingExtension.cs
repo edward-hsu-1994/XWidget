2708c32c3b779e93d6acc8a6b711533ac53fd01a
XuPeiYao <st89382000@gmail.com>
2019-03-20 13:53:36 +0800

    分頁轉換方法加入選擇方法參數

--- XWidget.Linq/PagingExtension.cs
+++ XWidget.Linq/PagingExtension.cs
@@ -16,8 +16,30 @@ namespace System.Linq {
         /// <param name="skip">起始索引</param>
         /// <param name="take">取得筆數</param>
         /// <returns>分頁結果</returns>
-        public static Paging<TSource> AsPaging<TSource>(this IEnumerable<TSource> source, int skip = 0, int take = 10) {
+        public static Paging<TSource> AsPaging<TSource>(
+            this IEnumerable<TSource> source,
+            int skip = 0,
+            int take = 10) {
             return new Paging<TSource>(source, skip, take);
+        }
+
+        /// <summary>
+        /// 將列舉項目轉換為分頁類型
+        /// </summary>
+        /// <typeparam name="TSource">元素類型</typeparam>
+        /// <param name="selector">對應方法</param>
+        /// <param name="source">分頁資料來源</param>
+        /// <param name="skip">起始索引</param>
+        /// <param name="take">取得筆數</param>
+        /// <returns>分頁結果</returns>
+        public static Paging<TSource> AsPaging<TSource>(
+            this IEnumerable<TSource> source,
+            Func<TSource, TSource> selector,
+            int skip = 0,
+            int take = 10) {
+            var result = new Paging<TSource>(source, skip, take);
+            result.Selector = selector;
+            return result;
         }
     }
 }
