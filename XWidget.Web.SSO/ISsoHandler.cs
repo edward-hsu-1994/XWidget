71d6e926a2a7901cfe254ae0e795ec9dbd1f905d
XuPeiYao <st89382000@gmail.com>
2019-03-25 15:31:40 +0800

    加入處理類別與多個SSO提供者DI方法

---
+++ XWidget.Web.SSO/ISsoHandler.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XWidget.Web.SSO {
+    /// <summary>
+    /// SSO處理介面
+    /// </summary>
+    public interface ISsoHandler {
+        /// <summary>
+        /// 使用第三方登入
+        /// </summary>
+        /// <param name="provider">SSO提供者</param>
+        /// <param name="token">存取權杖</param>
+        /// <param name="context">HttpContext</param>
+        /// <returns></returns>
+        Task OnLogin(ISsoProvider provider, string token, HttpContext context);
+
+        /// <summary>
+        /// 第三方登入失敗
+        /// </summary>
+        /// <param name="provider">SSO提供者</param>
+        /// <param name="context">HttpContext</param>
+        /// <returns></returns>
+        Task OnError(ISsoProvider provider, HttpContext context);
+    }
+}
