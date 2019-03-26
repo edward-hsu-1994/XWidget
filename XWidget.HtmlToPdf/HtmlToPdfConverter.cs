c587e9448dff8d48b4ed33f52261b82f8d5e1ebe
XuPeiYao <st89382000@gmail.com>
2019-03-26 21:58:08 +0800

    實作簡易的HTML轉PDF

---
+++ XWidget.HtmlToPdf/HtmlToPdfConverter.cs
@@ -0,0 +1,29 @@
+﻿using PuppeteerSharp;
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace XWidget.HtmlToPdf {
+    public class HtmlToPdfConverter {
+        Browser _browser;
+        /// <summary>
+        /// 瀏覽器
+        /// </summary>
+        /// <param name="browser">瀏覽器</param>
+        public HtmlToPdfConverter(Browser browser) {
+            _browser = browser;
+        }
+
+        /// <summary>
+        /// 將輸入的HTML
+        /// </summary>
+        /// <param name="html"></param>
+        /// <returns></returns>
+        public async Task<Stream> Convert(string html) {
+            var page = await _browser.NewPageAsync();
+            await page.SetContentAsync(html);
+            return await page.PdfStreamAsync();
+        }
+    }
+}
