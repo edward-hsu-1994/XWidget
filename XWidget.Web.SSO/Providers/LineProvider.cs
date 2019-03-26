84e0fb1c4118905b172330d230511773c1eb63e4
XuPeiYao <st89382000@gmail.com>
2019-03-26 17:15:46 +0800

    加入Line OAuth提供者

--- XWidget.Web.SSO/Providers/LineProvider.cs
+++ XWidget.Web.SSO/Providers/LineProvider.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,21 +8,21 @@ using Microsoft.Extensions.Primitives;
 using Newtonsoft.Json.Linq;
 
 namespace XWidget.Web.SSO.Providers {
-    public class GithubProvider : SsoProviderBase {
+    public class LineProvider : SsoProviderBase {
         private HttpClient client = new HttpClient();
 
-        public GithubProvider(DefaultProviderConfiguration<GithubProvider> config, IHttpClientFactory clientFactory) : base(config) {
+        public LineProvider(DefaultProviderConfiguration<LineProvider> config, IHttpClientFactory clientFactory) : base(config) {
             this.client = clientFactory.CreateClient();
         }
 
-        public override string Name => "Github";
+        public override string Name => "Line";
 
         public override async Task<string> GetLoginUrlAsync(HttpContext context) {
             var url = new UriBuilder();
-            url.Host = "github.com";
+            url.Host = "access.line.me";
             url.Scheme = "https";
-            url.Path = "/login/oauth/authorize";
-            url.Query = $"?client_id={Configuration.AppId}&redirect_uri={Uri.EscapeDataString(GetCallbackUrl(context))}&state={GenerateStateCode()}";
+            url.Path = "/oauth2/v2.1/authorize";
+            url.Query = $"?clientId={Configuration.AppId}&response_type=code&redirect_uri={Uri.EscapeDataString(GetCallbackUrl(context))}&state={GenerateStateCode()}";
 
             if (Configuration.Scopes != null && Configuration.Scopes.Count > 0) {
                 url.Query += "&scope=" + string.Join("%20", Configuration.Scopes);
@@ -37,17 +38,28 @@ namespace XWidget.Web.SSO.Providers {
                 return false;
             }
         }
+
         public override async Task<string> GetLoginCallbackTokenAsync(HttpContext context) {
             if (context.Request.Query.TryGetValue("code", out StringValues code)) {
                 try {
-                    var request = new System.Net.Http.HttpRequestMessage(new HttpMethod(HttpMethods.Post), $"https://github.com/login/oauth/access_token?client_id={Configuration.AppId}&client_secret={Configuration.AppKey}&code={code[0]}");
-                    request.Headers.TryAddWithoutValidation("accept", "application/json");
+                    var currentUrl = context.Request.GetAbsoluteUri().ToString();
+                    currentUrl = currentUrl.Split(new char[] { '?' }, 2)[0];
 
-                    var response = await client.SendAsync(request);
+                    var dict = new Dictionary<string, string>();
+                    dict.Add("grant_type", "authorization_code");
+                    dict.Add("code", code);
+                    dict.Add("redirect_uri", currentUrl);
+                    dict.Add("client_id", Configuration.AppId);
+                    dict.Add("client_secret", Configuration.AppKey);
 
-                    var responseString = await response.Content.ReadAsStringAsync();
+                    var req = new HttpRequestMessage(HttpMethod.Post, "https://api.line.me/oauth2/v2.1/token") { Content = new FormUrlEncodedContent(dict) };
+                    var res = await client.SendAsync(req);
 
-                    var responseJson = JObject.Parse(responseString);
+                    if (!res.IsSuccessStatusCode) {
+                        return null;
+                    }
+
+                    var responseJson = JObject.Parse(await res.Content.ReadAsStringAsync());
 
                     return responseJson["token_type"].Value<string>() + " " + responseJson["access_token"].Value<string>();
                 } catch {
