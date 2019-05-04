742236cfecb8899d451350072517a36cdc28d917
XuPeiYao <st89382000@gmail.com>
2019-05-04 22:14:39 +0800

    修正套件相依關係

--- XWidget.Web.Mvc.EFETag/EFETagMiddlewareExtension.cs
+++ XWidget.Web.Mvc.EFETag/EFETagMiddlewareExtension.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using StackExchange.Redis;
 using System;
 using System.Collections.Generic;
 using System.Text;
