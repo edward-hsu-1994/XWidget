2972d990802c4e0a4f6657f0317d1280edf18ccc
XuPeiYao <st89382000@gmail.com>
2019-06-13 21:56:34 +0800

    加入屬性掛勾類別庫

---
+++ XWidget.PropertyHook/PropertyHookInjector.Interceptor.cs
@@ -0,0 +1,16 @@
+﻿using Castle.DynamicProxy;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace XWidget.PropertyHook {
+    public partial class PropertyHookInjector<T> {
+        internal class PropertyHookInterceptor<T2> : IInterceptor {
+            public T2 OrigionObject { get; set; }
+
+            public void Intercept(IInvocation invocation) {
+                invocation.Proceed();
+            }
+        }
+    }
+}
---
+++ XWidget.PropertyHook/PropertyHookInjector.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace XWidget.PropertyHook {
+    public delegate void PropertyCallback<T, TProperty>(T sender, TProperty oldValue, TProperty newValue);
+    public partial class PropertyHookInjector<T> {
+        public T OrigionObject { get; private set; }
+
+        private PropertyHookInterceptor<T> Interceptor { get; set; }
+
+        public PropertyHookInjector(T origionObject) {
+            OrigionObject = origionObject;
+            Interceptor.OrigionObject = origionObject;
+        }
+
+        public PropertyHookInjector<T> HookGetBeforeProperty<TProperty>(
+            Expression<Func<T, object>> selector,
+            PropertyCallback<T, TProperty> callback) {
+            throw new NotImplementedException();
+        }
+
+        public PropertyHookInjector<T> HookGetAfterProperty<TProperty>(
+            Expression<Func<T, object>> selector,
+            PropertyCallback<T, TProperty> callback) {
+            throw new NotImplementedException();
+        }
+
+        public PropertyHookInjector<T> HookSetBeforeProperty<TProperty>(
+            Expression<Func<T, object>> selector,
+            PropertyCallback<T, TProperty> callback) {
+            throw new NotImplementedException();
+        }
+
+        public PropertyHookInjector<T> HookSetAfterProperty<TProperty>(
+            Expression<Func<T, object>> selector,
+            PropertyCallback<T, TProperty> callback) {
+            throw new NotImplementedException();
+        }
+
+        public T Inject() {
+            throw new NotImplementedException();
+        }
+    }
+}
